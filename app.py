6e695269fd169c1a4912b2af1b567409618743f9
Judd Maltin <judd@newgoliath.com>
2025-04-18 10:36:12 -0400

    database started

--- app.py
+++ app.py
@@ -1,18 +1,37 @@
-from flask import Flask
+from flask import Flask, render_template, request
 from markupsafe import escape
-from flask import render_template
+import sqlite3
+
+con = sqlite3.connect("assignments.db")
+cur = con.cursor()
+cur.execute(
+    "CREATE TABLE IF NOT EXISTS assignments(name);"
+)
 
 app = Flask(__name__)
 
-@app.route('/')
-def show_assignment_input(name=None):
-    return render_template('assignment_input.html')
+@app.route("/")
+def index():
+    return render_template('index.html')
     # here we present the form
 
-@app.route('/new_assignment')
-def add_assignmnt():
-    pass
-    
+@app.route('/new_assignment', methods=("GET","POST"))
+def new_assignment():
+    if request.method == "POST":
+        new_assignment = {"name": request.form['assignment_name']}
+        # save_assignment(new_assignment)   
+        return render_template("show_assignment.html", new_assignment=new_assignment)
+    return render_template("index.html")
+
+def save_assignment(assignment):
+    con = sqlite3.connect("assignments.db")
+    cur = con.cursor()
+    cur.execute(
+        "INSERT INTO assignments(name) VALUES (?);",
+        (assignment["name"])
+    )
+    con.commit()
+    return
 
 @app.route('/hello/')
 @app.route('/hello/<name>')
